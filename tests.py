6d6559304cbe77d436bf6d26ef9247e3a4b1f58f
Alexey Pashkin <pashalexalex@yandex.ru>
2026-07-22 02:08:25 +0400

    new_commit

--- tests.py
+++ tests.py
@@ -20,5 +20,5 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
+
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
